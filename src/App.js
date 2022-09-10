a628a548dd84a729ee6fdfb9f809e91f48cf72cc
Mihai Laurentiu Iordache <adiordache61@gmail.com>
2022-09-10 08:59:56 +0200

    - Added i18n - Added react-router-dom@5 - Added support for en and es

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 
 import { useSelector } from 'react-redux';
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
+
+import en from './lang/en';
+import es from './lang/es';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    es: {
+      translation: es,
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 function App() {
   const msg = useSelector((state) => state.Auth.sample);
+  const { t } = useTranslation();
 
-  return <div className='App'>{msg}</div>;
+  return <div className='App'>{t('hello')}</div>;
 }
 
 export default App;
